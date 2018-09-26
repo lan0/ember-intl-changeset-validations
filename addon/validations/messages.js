8af7df7d1ed3f1d247557b519cced63e6300d170
Michal Bryxí <michal.bryxi@gmail.com>
2018-09-26 19:33:41 +0100

    Just saving my progress

--- addon/validations/messages.js
+++ addon/validations/messages.js
@@ -2,16 +2,24 @@ import defaultMessages from 'ember-changeset-validations/utils/messages';
 import application from '../utils/application';
 
 const { keys } = Object;
-const i18n = application.instance.lookup('service:intl');
+const intl = application.instance.lookup('service:intl');
 
 let messages = {};
 keys(defaultMessages).forEach(key => {
+  console.log("here");
   let lookupKey = `validations.${key}`;
-  if (i18n.exists(lookupKey)) {
-    messages[key] = i18n.t(lookupKey).toString(); // use the translation
+  if (intl.exists(lookupKey)) {
+    // messages[key] = (a,b,c,d) => {
+      // console.log("over hter", a,b,c,d);
+      // messages[key] = intl.t(lookupKey, {min: '{min}'}).toString(); // use the translation
+      // messages[key] = messages
+    // }
   } else {
     messages[key] = defaultMessages[key];
   }
 });
 
+console.log(messages);
+
 export default messages;
+// export default [];
