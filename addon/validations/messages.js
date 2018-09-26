5c36e59ac16d37fa537fed596f4b1e6ecb88ccb8
Michal Bryxí <michal.bryxi@gmail.com>
2018-09-26 19:33:41 +0100

    Dynamic keys
     - Translation can have placeholders and they are properly interpreted
     - Add dummy test app

--- addon/validations/messages.js
+++ addon/validations/messages.js
@@ -2,13 +2,13 @@ import defaultMessages from 'ember-changeset-validations/utils/messages';
 import application from '../utils/application';
 
 const { keys } = Object;
-const i18n = application.instance.lookup('service:intl');
+const intl = application.instance.lookup('service:intl');
 
 let messages = {};
 keys(defaultMessages).forEach(key => {
   let lookupKey = `validations.${key}`;
-  if (i18n.exists(lookupKey)) {
-    messages[key] = i18n.t(lookupKey).toString(); // use the translation
+  if (intl.exists(lookupKey)) {
+    messages[key] = intl.lookup(lookupKey);
   } else {
     messages[key] = defaultMessages[key];
   }
