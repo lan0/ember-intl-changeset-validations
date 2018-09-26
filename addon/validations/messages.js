c2949f9b3301ffec2061736276bd4ca985675f95
Michal Bryxí <michal.bryxi@gmail.com>
2018-09-26 14:17:23 +0100

    Initial implementation
    - Initial code proudly taken from: https://github.com/mirai-audio/ember-i18n-changeset-validations
    - Code works and does the translation

---
+++ addon/validations/messages.js
@@ -0,0 +1,20 @@
+import defaultMessages from 'ember-changeset-validations/utils/messages';
+import application from '../utils/application';
+
+const { keys } = Object;
+const i18n = application.instance.lookup('service:intl');
+
+let messages = {};
+keys(defaultMessages).forEach(key => {
+  let lookupKey = `validations.${key}`;
+  if (i18n.exists(lookupKey)) {
+    messages[key] = i18n.t(lookupKey).toString(); // use the translation
+  } else {
+    messages[key] = defaultMessages[key];
+  }
+});
+
+console.log('MOOOOOOO');
+console.log(messages);
+
+export default messages;
---
+++ app/validations/messages.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-intl-changeset-validations/validations/messages';
