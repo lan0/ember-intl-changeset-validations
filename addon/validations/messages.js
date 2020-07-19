a45b5e8096e8dd3e53d43a56134406baf3be487d
Michal Bryxí <michal.bryxi@gmail.com>
2020-07-19 20:42:25 +0100

    fix: ember-changeset-validations v3.x and ember-intl v5.x compatibility

--- addon/validations/messages.js
+++ addon/validations/messages.js
@@ -8,7 +8,15 @@ let messages = {};
 keys(defaultMessages).forEach(key => {
   let lookupKey = `validations.${key}`;
   if (intl.exists(lookupKey)) {
-    messages[key] = intl.lookup(lookupKey);
+    messages[key] = intl.lookup(lookupKey).reduce(
+      (acc, curr) => {
+        if(curr.type === 1) {
+          return acc + `{${curr.value}}`;
+        } else {
+          return acc+curr.value
+        }
+      }, ""
+    );
   } else {
     messages[key] = defaultMessages[key];
   }
