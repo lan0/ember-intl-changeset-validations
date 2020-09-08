92f39b412df373b9c7597a680f0dc7456708e3f5
BnitoBzh <fievet.benoit@gmail.com>
2020-09-08 14:55:10 +0000

    Fix `lookup` function signature compatibility, ember-intl >= 5.4.0

--- addon/validations/messages.js
+++ addon/validations/messages.js
@@ -4,19 +4,21 @@ import application from '../utils/application';
 const { keys } = Object;
 const intl = application.instance.lookup('service:intl');
 
-let messages = {};
+const messages = {};
+
 keys(defaultMessages).forEach(key => {
-  let lookupKey = `validations.${key}`;
+  const lookupKey = `validations.${key}`;
   if (intl.exists(lookupKey)) {
-    messages[key] = intl.lookup(lookupKey).reduce(
+    const lookupResult = intl.lookup(lookupKey);
+    messages[key] = 'string' !== typeof lookupResult ? lookupResult.reduce(
       (acc, curr) => {
-        if(curr.type === 1) {
+        if (curr.type === 1) {
           return acc + `{${curr.value}}`;
         } else {
-          return acc+curr.value
+          return acc + curr.value
         }
       }, ""
-    );
+    ) : lookupResult;
   } else {
     messages[key] = defaultMessages[key];
   }
